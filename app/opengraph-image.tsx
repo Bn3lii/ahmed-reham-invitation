0f76fe9bd63d0ffe1f678b7441654dd9f2066b9d
devbn3li <muhammed.ali.hissein@gmail.com>
2026-07-31 23:40:19 +0300

    feat: add Open Graph and Twitter metadata images with supporting font asset

--- app/opengraph-image.tsx
+++
@@ -1,9 +0,0 @@
-import { ogAlt, ogContentType, ogSize, renderOgImage } from "@/lib/og-image";
-
-export const alt = ogAlt;
-export const size = ogSize;
-export const contentType = ogContentType;
-
-export default async function OpengraphImage() {
-  return renderOgImage();
-}
